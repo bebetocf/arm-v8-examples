2a629f4fe6067ec146d111fb93bbc0ecacde14d0
Roberto <betocostaf@gmail.com>
2020-08-13 09:54:27 -0300

    adding q4

--- q4/main.c
+++ q4/main.c
@@ -2,15 +2,11 @@
 
 #include <stdio.h>
 
-void test(long long int *a, long long int *b, long long int *m);
+void test();
 
 int main(void)
 {
-    long long int a = 9, b = 10;
-    long long int m = 8;
-    test(&a, &b, &m);
-    printf("a = %lld\n", a);
-    printf("b = %lld\n", b);
-    printf("m = %lld\n", m);
+
+    test();
     return 0;
 }
