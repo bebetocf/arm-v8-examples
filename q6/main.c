c8a6bb949792fa2edfa15ed5663a092c48e190f3
Roberto <betocostaf@gmail.com>
2020-08-13 10:11:18 -0300

    adding q6

--- q6/main.c
+++ q6/main.c
@@ -2,15 +2,14 @@
 
 #include <stdio.h>
 
-void test(long long int *a, long long int *b, long long int *m);
+long long int test(char *a);
 
 int main(void)
 {
-    long long int a = 9, b = 10;
-    long long int m = 8;
-    test(&a, &b, &m);
-    printf("a = %lld\n", a);
-    printf("b = %lld\n", b);
-    printf("m = %lld\n", m);
+    char a[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    long long int x = 0;
+    x = test(a);
+    printf("a = %s\n", a);
+    printf("x = %lld\n", x);
     return 0;
 }
--- q6/test.s
+++ q6/test.s
@@ -1,23 +1,25 @@
 	.globl test
 test:
-	LDUR X9, [X0, #0]
-	LDUR X10, [X1, #0]
-	LDUR X11, [X2, #0]
-	LDR X12, =5
-	ADD X9, XZR, X12 // a = 5
-	LDR X12, =4
-	ADD X10, XZR, X12 // b = 4
-	LDR X12, =12
-	ADD X11, XZR, X12 // m = 12
-	ADD X11, X9, #0 // m = a
-	CMP X10, X11 // b == m
-	B.NE Else
-	SUB X11, X10, X9 // m = b - a
-	B Exit
-	Else:
-	SUB X11, X9, X10 // m = a - b
+
+	ADD X9, XZR, XZR // i = 0 + 0
+	ADD X18, XZR, XZR // X18 = 0 + 0
+	L1:  ADD X10, X0, X9 // endereço de str[i] em X10
+	LDRSB X11, [X10, #0] // X11 = str[i]
+	CBZ X11, Exit // se str[i] == 0, acabou a string
+	ADD X9, X9, #1 // i = i + 1
+	CMP X11, #65 // X11 == A
+	B.EQ Cont
+	CMP X11, #69 // X11 == E
+	B.EQ Cont
+	CMP X11, #73 // X11 == I
+	B.EQ Cont
+	CMP X11, #79 // X11 == O
+	B.EQ Cont
+	CMP X11, #85 // X11 == U
+	B.NE L1
+	Cont: ADD X18, X18, #1 // X18 = X18 + 1
+	B L1
 	Exit:
-	STUR X9, [X0, #0]
-	STUR X10, [X1, #0]
-	STUR X11, [X2, #0]
+	ADD X0, XZR, X18
+
 	BR	X30
