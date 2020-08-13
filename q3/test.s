2047251c88d2e2bfa4e3b5b066a806b2a76af46a
Roberto <betocostaf@gmail.com>
2020-08-13 09:50:36 -0300

    adding q3

--- q3/test.s
+++ q3/test.s
@@ -1,23 +1,9 @@
 	.globl test
 test:
-	LDUR X9, [X0, #0]
-	LDUR X10, [X1, #0]
-	LDUR X11, [X2, #0]
-	LDR X12, =5
-	ADD X9, XZR, X12 // a = 5
-	LDR X12, =4
-	ADD X10, XZR, X12 // b = 4
-	LDR X12, =12
-	ADD X11, XZR, X12 // m = 12
-	ADD X11, X9, #0 // m = a
-	CMP X10, X11 // b == m
-	B.NE Else
-	SUB X11, X10, X9 // m = b - a
-	B Exit
-	Else:
-	SUB X11, X9, X10 // m = a - b
-	Exit:
-	STUR X9, [X0, #0]
-	STUR X10, [X1, #0]
-	STUR X11, [X2, #0]
+
+	LDR X15, =0xffffffffffffffff
+	ADD X11, XZR, X15
+	UBFX X12, X10, #11, #6 // X12 = X10[16:11]
+	LSR X12, X12, #11 // X12 >> 11
+	BFI X11, X10, #26, #6 // X11[31:26] = X10[16:11]
 	BR	X30
